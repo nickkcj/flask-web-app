f4aac19a2e79462a8c144bf8296af459387d2903
Nickkcj <nickcj123@hotmail.com>
2025-02-10 21:24:32 -0300

    html/bootstrap/css start

--- flaskblog.py
+++ flaskblog.py
@@ -1,12 +1,27 @@
-from flask import Flask
+from flask import Flask, render_template, url_for
 
 app = Flask(__name__)
+
+posts = [
+    {
+        'author': 'Corey Schafer',
+        'title': 'Blog Post 1',
+        'content': 'First Post Content',
+        'date_posted': 'April 20, 2018'
+    },
+    {
+        'author': 'Ana Beatriz',
+        'title': 'Blog Post 2',
+        'content': 'Second Post Content',
+        'date_posted': 'April 15, 2015'
+    }
+]
 
 @app.route("/")
 @app.route("/home")
 def home_page():
-    return "<h1>Home Page</h1>"
+    return render_template('home.html', posts=posts) #Here we are turning available the posts data to be used in the html file.
 
 @app.route("/about")
 def about_page():
-    return "<h1>About Page</h1>"
+    return render_template('about.html', title='About')
