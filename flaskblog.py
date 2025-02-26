43364474c659d6523b94f1854d38e04e72abbd2a
Nickkcj <nickcj123@hotmail.com>
2025-02-25 22:42:04 -0300

    create user table

--- flaskblog.py
+++ flaskblog.py
@@ -3,11 +3,19 @@ from forms import RegistrationForm, LoginForm
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
-
 app.config['SECRET_KEY'] = 'a069dcf1fafbc38d6e69f8aedebd817b'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db' #Relative path from the current file
-
 db = SQLAlchemy(app)
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(20), unique=True, nullable=False)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+    image_file = db.Column(db.String(20), nullable=False, default='default.jpg') #Default image
+    password = db.Column(db.String(60), nullable=False)
+    
+    def __repr__(self):
+        return f"User('{self.username}', '{self.email}', '{self.image_file}')"
 
 posts = [
     {
