c964da9a167aade0a0775881712e0f31c99a18d8
Nickkcj <nickcj123@hotmail.com>
2025-03-07 18:44:56 -0300

    adding CRUD to posts

--- flaskblog/routes/blog_routes.py
+++ flaskblog/routes/blog_routes.py
@@ -1,5 +1,5 @@
 import secrets, os
-from flask import render_template, url_for, flash, redirect, Blueprint, request, current_app
+from flask import render_template, url_for, flash, redirect, Blueprint, request, current_app, abort
 from flaskblog.forms import RegistrationForm, LoginForm, UpdateAccountForm, PostForm
 from flaskblog.models import User, Post
 from flask_login import login_user, current_user, logout_user, login_required
@@ -100,4 +100,29 @@ def new_post():
         db.session.commit()
         flash('Your post has been created!', 'success')
         return redirect(url_for('routes.home_page'))
-    return render_template('create_post.html', title='New Post', form=form)
+    return render_template('create_post.html', title='New Post', 
+                           form=form, legend='New Post')
+
+@routes.route("/post/<int:post_id>")
+def post(post_id):
+    post = Post.query.get_or_404(post_id)
+    return render_template('post.html', title=post.title, post=post)
+
+@routes.route("/post/<int:post_id>/update", methods=['GET', 'POST'])
+@login_required
+def update_post(post_id):
+    post = Post.query.get_or_404(post_id)
+    if post.author != current_user:
+        abort(403)
+    form = PostForm()
+    if form.validate_on_submit():
+        post.title = form.title.data
+        post.content = form.content.data
+        db.session.commit()
+        flash('Your post has been updated!', 'success')
+        return redirect(url_for('routes.post', post_id=post.id))
+    elif request.method == 'GET':
+        form.title.data = post.title
+        form.content.data = post.content
+    return render_template('create_post.html', title='Update Post', 
+                           form=form, legend='Update Post')
